a0326fac03b01e4a19ea2bee637629504c9fa459
Deepak Malhotra <deepakm.0908@outlook.com>
2026-09-14 19:24:49 +0100

    further renaming for clarity, added gitignore etc

--- src/metrics.py
+++ src/metrics.py
@@ -10,17 +10,17 @@ def pred(X, beta):
 def residuals(y, y_pred):
     return y - y_pred
 
-def RSS(y, y_pred):
+def rss(y, y_pred):
     return np.dot(residuals(y, y_pred), residuals(y, y_pred))
 
-def MSE(y, y_pred):
+def mse(y, y_pred):
     N = len(y)
-    return 1/N * RSS(y, y_pred)
+    return 1/N * rss(y, y_pred)
 
-def Rsquare(y, y_pred):
+def rsquare(y, y_pred):
     ybar = np.mean(y)
-    RSS0 = RSS(y, ybar)
-    return 1 - RSS(y, y_pred)/RSS0
+    RSS0 = rss(y, ybar)
+    return 1 - rss(y, y_pred)/RSS0
 
 def least_squares(Y, X):
     XtX = np.matmul(np.transpose(X), X)
